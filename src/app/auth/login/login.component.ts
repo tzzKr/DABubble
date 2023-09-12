eae7986bfe4d21bce5d409763eef2ec111bfaeff
Sebastian Köhler <mailqkoehler-sebastian.de>
2023-09-12 19:26:39 +0200

    try to implement googleAuth

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
 
@@ -10,8 +11,19 @@ import { AuthService } from 'src/app/core/services/auth.service';
 export class LoginComponent implements OnInit {
   constructor(
     public route: ActivatedRoute,
-    public authService: AuthService
-  ) { }
-  ngOnInit() { }
+    public authService: AuthService,
+    public afAuth: AngularFireAuth
+
+  ) { 
+
+
+  }
+  ngOnInit() {
+   }
+
+   login(){
+   this.authService.GoogleAuth();
+   }
 
 }
+
